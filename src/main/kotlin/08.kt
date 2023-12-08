91f409fa70131b9160a963b80b1050dd2fc36869
MrsCf28 <rebeccaflynn28@outlook.com>
2023-12-08 14:52:11 +0000

    day 8

--- src/main/kotlin/08.kt
+++ src/main/kotlin/08.kt
@@ -1,15 +1,38 @@
-private val input = readFileAsLines("08.1.sample.txt")
-// private val input = readFileAsLines("08.txt")
+//private val input = readFileAsLines("08.sample.txt")
+private val input = readFileAsLines("08.txt")
 
-fun main(args: Array<String>) {
-    println(partOne(input))
-    println(partTwo(input))
+fun main() {
+    val directions = input[0]
+    val locationMap = input.subList(2, input.size).map {
+        val (location, left, right) = it.split(" = (", ", ")
+        location to Location(left, right.dropLast(1))
+    }.toMap()
+
+    println(partOne(directions, locationMap))
+    println(partTwo(directions, locationMap))
 }
 
-private fun partOne(input: List<String>): Int {
-    return 0
-}
+class Location(val left: String, val right: String) {}
 
-private fun partTwo(input: List<String>): Int {
-    return 0
+private fun partOne(directions: String, locationMap: Map<String, Location>): Long =
+    findCount("AAA", directions, locationMap, "ZZZ")
+
+private fun partTwo(directions: String, locationMap: Map<String, Location>): Long =
+    locationMap.keys
+        .filter { it.endsWith('A') }
+        .map { findCount(it, directions, locationMap, "Z") }
+        .reduce { acc, next -> findLCM(acc, next) }
+
+fun findCount(start: String, directions: String, locationMap: Map<String, Location>, ending: String): Long {
+    var count = 0L
+    var location = start
+
+    while (!location.endsWith(ending)) {
+        for (direction in directions) {
+            location = if (direction == 'L') locationMap[location]!!.left else locationMap[location]!!.right
+            count++
+        }
+    }
+
+    return count
 }
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -9,3 +9,14 @@ fun intList(string: String): List<Int> {
 fun longList(string: String): List<Long> {
     return string.trim().split(" ").filter { it.isNotEmpty() }.map { it.toLong() }
 }
+
+fun findLCM(a: Long, b: Long): Long {
+    val larger = if (a > b) a else b
+    val maxLcm = a * b
+    var lcm = larger
+    while (lcm < maxLcm) {
+        if (lcm % a == 0L && lcm % b == 0L) return lcm
+        lcm += larger
+    }
+    return maxLcm
+}
