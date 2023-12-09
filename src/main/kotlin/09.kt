59a2464e0812b7d58dc652e991cad377e8883022
MrsCf28 <rebeccaflynn28@outlook.com>
2023-12-09 20:49:08 +0000

    day 9

--- src/main/kotlin/09.kt
+++ src/main/kotlin/09.kt
@@ -1,15 +1,26 @@
-private val input = readFileAsLines("09.sample.txt")
-// private val input = readFileAsLines("09.txt")
+//private val input = readFileAsLines("09.sample.txt")
+private val input = readFileAsLines("09.txt")
 
 fun main() {
     println(partOne(input))
     println(partTwo(input))
 }
 
-private fun partOne(input: List<String>): Int {
-    return 0
-}
+private fun partOne(input: List<String>): Long = findSumOfTerms(input, true)
+private fun partTwo(input: List<String>): Long = findSumOfTerms(input, false)
 
-private fun partTwo(input: List<String>): Int {
-    return 0
-}
+fun findDiffSequence(sequence: List<Long>): List<Long> = List(sequence.size) { i ->
+    if (i + 1 < sequence.size) sequence[i + 1] - sequence[i] else 0L
+}.dropLast(1)
+
+fun findSumOfTerms(input: List<String>, next: Boolean): Long =
+    input.sumOf { line ->
+        val sequence = if (next) longList(line) else longList(line).reversed()
+        var sequenceDiff = findDiffSequence(sequence)
+        val sequenceHistory = mutableListOf(sequence, sequenceDiff)
+        while (sequenceDiff.toSet().size != 1) {
+            sequenceDiff = findDiffSequence(sequenceDiff)
+            sequenceHistory.add(sequenceDiff)
+        }
+        sequenceHistory.sumOf { it.last() }
+    }
